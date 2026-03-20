524ce54e63b0051dce0bbafd6926f7b41e0af1aa
taran1s <taran1sdev@gmail.com>
2026-03-20 09:42:51 +0000

    Basic game functionality implemented

---
+++ typingGame.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+// ANSI colour codes
+#define RED   "\033[1;31m"
+#define BLUE  "\033[1;34m"
+#define RESET "\033[0m"
+
+// Cursor movement
+#define LEFT    "\033[1D"
+#define RIGHT   "\033[1C"
+#define SAVE    "\033[s"
+#define RESTORE "\033[u"
+
+// Key codes
+#define BACKSPACE 127
+#define ESC       27
+
+struct termios original_settings;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_settings);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &original_settings);
+
+    atexit(disableRawMode);
+
+    struct termios raw = original_settings;
+
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int readFile(char **buf) {
+    FILE *input = fopen("example.txt", "r");
+    if (!input) return 0;
+
+    fseek(input, 0, SEEK_END);
+    long size = ftell(input);
+    rewind(input);
+    
+    *buf = malloc(size + 1);
+    if (*buf == NULL) {
+        fclose(input);
+        return 0;
+    }
+
+    fread(*buf, 1, size, input);
+    (*buf)[size] = '\0';
+
+    fclose(input);
+    return size;
+}
+
+void setup(char *gameText) {
+    printf(SAVE "%s" RESET RESTORE, gameText);
+    printf(RESTORE);
+    fflush(stdout);
+}
+
+void start(char* gameText, int fileSize) {
+   long index = 0;
+   int errors = 0;
+   
+   while(index < fileSize) {
+      char input = getchar();
+
+      if (input == ESC) break;
+
+      if (input == BACKSPACE) {
+        if (index >= 0) { 
+            index--;            
+            printf(LEFT "%c" RESET LEFT, gameText[index]);
+            index--;
+        }
+      } else {
+          if (input == gameText[index]) {
+              printf(BLUE "%c" RESET, input);              
+          } else {
+              printf(RED "%c" RESET, input);
+              errors++;
+          }
+      }
+
+      fflush(stdout);
+      index++;
+   } 
+}
+
+int main(void) {
+    char *gameText = NULL;
+    int fileSize;
+
+    fileSize= readFile(&gameText);
+    
+    if (!fileSize) {
+        free(gameText);
+        printf("File size is 0..\n");
+        return 1;
+    }
+    
+    enableRawMode();
+    
+    setup(gameText);
+    
+    start(gameText, fileSize); 
+
+    free(gameText);
+}
